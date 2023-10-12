6727eab617219019d8b628a9651507516d9e1f41
StefanoMela <stefano.mela25@gmail.com>
2023-10-12 18:03:55 +0200

    small fixes

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,52 +1,52 @@
 <div class="footer-container">
     <div class="footer__divider">
-      <div class="fc_link_group">
-        <ul class="fc_first_link_group">
-          <li class="list-title" style="color: white; font-weight: bold">
-            DC COMICS
-          </li>
-          <li>
-           prova
-          </li>
-        </ul>
-        <ul class="fc_second_link_group">
-          <li class="list-title" style="color: white; font-weight: bold">
-            SHOP
-          </li>
-          <li>
-           prova
-          </li>
-        </ul>
-        <ul class="fc_third_link_group">
-          <li class="list-title" style="color: white; font-weight: bold">DC</li>
-          <li >
-           prova
-          </li>
-        </ul>
-        <ul class="fc_fourth_link_group">
-          <li class="list-title" style="color: white; font-weight: bold">
-            SITES
-          </li>
-          <li>
-           prova
-          </li>
-        </ul>
-      </div>
-      <div class="fc_img_overlay_container">
-        <img src="../assets/img/dc-logo-bg.png" alt="" />
-      </div>
+        <div class="fc_link_group">
+            <ul class="fc_first_link_group">
+                <li class="list-title">
+                    DC COMICS
+                </li>
+                <li>
+                    prova
+                </li>
+            </ul>
+            <ul class="fc_second_link_group">
+                <li class="list-title">
+                    SHOP
+                </li>
+                <li>
+                    prova
+                </li>
+            </ul>
+            <ul class="fc_third_link_group">
+                <li class="list-title">DC</li>
+                <li>
+                    prova
+                </li>
+            </ul>
+            <ul class="fc_fourth_link_group">
+                <li class="list-title">
+                    SITES
+                </li>
+                <li>
+                    prova
+                </li>
+            </ul>
+        </div>
+        <div class="fc_img_overlay_container">
+            <img src="../assets/img/dc-logo-bg.png" alt="" />
+        </div>
     </div>
     <div class="bottom-footer">
-      <div class="fc_btn_container">
-        <button class="call__to__action__button">Sign Up Now</button>
-      </div>
-      <div class="fc_social_icons_container">
-        <span class="hl-text">FOLLOW US</span>
-        <img src="public/assets/images/footer-facebook.png" alt="" />
-        <img src="public/assets/images/footer-twitter.png" alt="" />
-        <img src="public/assets/images/footer-youtube.png" alt="" />
-        <img src="public/assets/images/footer-pinterest.png" alt="" />
-        <img src="public/assets/images/footer-periscope.png" alt="" />
-      </div>
+        <div class="fc_btn_container">
+            <button class="call__to__action__button">Sign Up Now</button>
+        </div>
+        <div class="fc_social_icons_container">
+            <span class="hl-text">FOLLOW US</span>
+            <img src="../../public/assets/images/footer-facebook" alt="" />
+            <img src="public/assets/images/footer-twitter.png" alt="" />
+            <img src="public/assets/images/footer-youtube.png" alt="" />
+            <img src="public/assets/images/footer-pinterest.png" alt="" />
+            <img src="public/assets/images/footer-periscope.png" alt="" />
+        </div>
     </div>
-  </div>
+</div>
